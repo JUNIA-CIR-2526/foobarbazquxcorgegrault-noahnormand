974ed316f94b6bbb89567afd2c06b29e54b359a8
Noah Normand <noah.normand@student.junia.com>
2025-09-21 16:05:35 +0200

    Add Foo class with properties and methods for managing Bar, Baz, Qux, Corge, and Grault

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,4 +1,60 @@
 package com.jad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
+    private Bar bar;
+    private List<Baz> bazs;
+    private Qux qux;
+    private Corge corge;
+    private List<Grault> graults;
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+        this.bazs = new ArrayList<>();
+        this.qux = new Qux();
+        this.graults = new ArrayList<>();
+    }
+
+    public void addBaz(Baz baz) {
+        this.bazs.add(baz);
+    }
+
+    public void addGrault() {
+        Grault grault = new Grault(this);
+        this.graults.add(grault);
+    }
+
+    public Bar getBar() {
+        return bar;
+    }
+
+    public List<Baz> getBazs() {
+        return bazs;
+    }
+
+    public Qux getQux() {
+        return qux;
+    }
+
+    public Corge getCorge() {
+        return corge;
+    }
+
+    public void setCorge(Corge corge) {
+        if (this.corge != null) {
+            this.corge.setFoo(null);
+        }
+
+        this.corge = corge;
+
+        if (corge != null) {
+            corge.setFoo(this);
+        }
+    }
+
+    public List<Grault> getGraults() {
+        return graults;
+    }
 }
